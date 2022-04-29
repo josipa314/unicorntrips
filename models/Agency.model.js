ed0f6e20ca1c1f04f090b3a29e00a7372f16f46c
didicozaur <moisin.diana@yahoo.com>
2022-04-29 20:08:31 +0200

    updated the agency.model

--- models/Agency.model.js
+++ models/Agency.model.js
@@ -1,16 +1,29 @@
 const { default: mongoose } = require("mongoose");
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const agencySchema = new Schema(
-    {
-        name: String,
-        description: String,
-        location: String,
-        rating: Number,
-    }
+  {
+    name: String,
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: String,
+      required: [true, "Password required"],
+    },
+    description: String,
+    location: String,
+    rating: Number,
+  },
+  {
+    timestamps: true,
+  }
 );
 
+const Agency = model("Agency", agencySchema);
 
-const Agency = model("Owner", agencySchema);
-
-module.exports = Owner;
+module.exports = Agency;
