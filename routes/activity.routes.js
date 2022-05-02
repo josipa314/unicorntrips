87a39df744698034bd03320b69537e6325b2ce78
Josipa314 <101945658+josipa314@users.noreply.github.com>
2022-05-02 18:04:32 +0200

    added crud to activities

--- routes/activity.routes.js
+++ routes/activity.routes.js
@@ -17,41 +17,20 @@ router.get("/", (req, res, next) => {
 });
 
 
-// READ ONE ACTIVITY DETAILS: display activity details
-router.get("/:activityId", (req, res, next) => {
-    const id = req.params.activityId;
-
-    Activity.findById(id)
-        .then((activityDetails) => {
-            console.log(activityDetails)
-            res.render("activities/activity-details", activityDetails);
-        })
-        .catch(err => {
-            console.log("error getting activity details from DB", err)
-            next(err);
-        });
-})
 
 // CREATE ONE NEW ACTIVITY: render form
 router.get("/create",(req, res, next) => {
-/*     Agency.find()
+    Agency.find()
         .then((agenciesArr) => {
             res.render("activities/activity-create", {agencies: agenciesArr});
         })
         .catch(err => {
             console.log("error getting agencies from DB", err)
             next(err);
-        });   */  
-        Activity.find()
-        .then((activitiesArr) => {
-            console.log(activitiesArr);
-            res.render("activities/activity-create", { activities: activitiesArr });
+        });   
+/*             res.render("activities/activity-create"); */
           })
-         .catch((err) => {
-            console.log("error getting activities from DB", err);
-            next(err);
-          });
-})
+        
 
 /* 
 // CREATE ONE NEW ACTIVITY: process form
@@ -76,6 +55,83 @@ router.post("/create", (req, res, next) => {
         });
 
 }) */
+
+
+// UPDATE ONE ACTIVITY: display form
+router.get("/:activityId/edit", (req, res, next) => {
+    const id = req.params.activityId;
+    Activity.findByIdAndUpdate(id)
+        .then((activityDetails) => {
+            res.render("activities/activity-edit", activityDetails);
+        })
+        .catch(err => {
+            console.log("error getting activity details from DB", err)
+            next(err);
+        });
+});
+
+// UPDATE ONE ACTIVITY:: process form
+router.post("/:activityId/edit", (req, res, next) => {
+    const id = req.params.activityId;
+    const newDetails = {
+        title: req.body.title,
+        description: req.body.description,
+        agency: req.body.agency,
+        location: req.body.location,
+        rating: req.body.rating,
+        price: req.body.price,
+    };
+
+    Activity.findByIdAndUpdate(id, newDetails)
+        .then((activityFromDB) => {
+            res.redirect(`/activities/${activityFromDB._id}`);
+        })
+        .catch(err => {
+            console.log("error updating activity in DB", err)
+            next(err);
+        });
+});
+
+// READ ONE ACTIVITY DETAILS: display activity details
+router.get("/:activityId", (req, res, next) => {
+    const id = req.params.activityId;
+
+    Activity.findById(id)
+        .then((activityDetails) => {
+            console.log(activityDetails)
+            res.render("activities/activity-details", activityDetails);
+        })
+        .catch(err => {
+            console.log("error getting activity details from DB", err)
+            next(err);
+        });
+})
+
+// UPDATE ONE ACTIVITY: process form
+router.post("/:activityId/edit", (req, res, next) => {
+
+    const id = req.params.activityId;
+
+    const newDetails = {
+        title: req.body.title,
+        description: req.body.description,
+        agency: req.body.agency,
+        location: req.body.location,
+        rating: req.body.rating,
+        price: req.body.price,
+    };
+
+    Activity.findByIdAndUpdate(id, newDetails)
+        .then((activityFromDB) => {
+            console.log(activityFromDB);
+            res.redirect(`/activities/${activityFromDB._id}`);
+        })
+        .catch(err => {
+            console.log("error updating activity in DB", err)
+            next(err);
+        });
+});
+
 
 // DELETE that activity:
 router.post("/:activityId/delete",(req, res, next) => {
